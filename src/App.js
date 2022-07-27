5560c8f0de414d451dcfdcfc260959f182bb4f18
Ruge Li <rugeli0605@gmail.com>
2022-07-26 21:44:01 -0700

    start working on multi pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { Link } from "react-router-dom";
 import './App.css';
+import Quiz from './pages/Quiz';
+import Leaderboard from './pages/Leaderboard';
+import Authentication from './pages/Authentication';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className="App">
+        <h1>Welcome to React Router!</h1>
+        {/* <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes> */}
+        <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/Authentication">Sign up/Log in</Link> |{" "}
+        <Link to="/Quiz">Play</Link>
+        </nav>
+      </div>
+    );
+  }
 
 export default App;
---
+++ src/pages/Quiz.js
@@ -0,0 +1,11 @@
+import App from '/App';
+
+function Quiz() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Play</h2>
+    </main>
+  );
+}
+
+export default Quiz;
